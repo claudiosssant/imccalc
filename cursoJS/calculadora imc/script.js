3a397fb26cf82b305732a3de8fcb9c99b3aeec7d
Claudio Santos <claudiogsssantos@gmail.com>
2023-04-12 21:45:10 -0300

    Update JS

--- cursoJS/calculadora imc/script.js
+++ cursoJS/calculadora imc/script.js
@@ -14,20 +14,15 @@ function calcular(event){
     resultado = document.getElementById('resultado');
 
    if(imc < 17){
-    
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Cuidado você está muito abaixo do peso!' 
+    alert(`Seu resultado foi: ${imc}, você está muito abaixo do peso!!!`) 
    }else if(imc > 17 && imc <= 18.49){
-    
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Cuidado você está abaixo do peso!'
+    alert(`Seu resultado foi: ${imc}, você está abaixo do peso!`)
    }else if(imc >=18.5 && imc < 24.99){
-    
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> VocÊ está no peso ideal!'
+    alert(`Seu resultadom foi: ${imc}, você está no peso ideal!`)
    }else if(imc > 25 && imc <= 29.99){
-    
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Cuidado você está acima do peso!'
+    alert(`Seu resultado foi: ${imc}, você está acima do peso!`)
    }else if(imc >= 30){
-    
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Cuidado você está com obesidade!'
+    alert(`Seu resultado foi: ${imc}, você está obeso!!!`)
    }
 
 
